a26bc4bdbc0af4be6e95801fd656f1abe398c163
chenyue <chenyue@leinao.ai>
2019-05-05 19:00:13 +0800

    添加日志

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
+
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
+app.use(morgan('short', {stream: accessLogStream}));
 
 app.use(express.static('./public'));
 
