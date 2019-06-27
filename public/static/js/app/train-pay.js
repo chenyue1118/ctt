0a65544c2fc6bf95972c3d9a47475e72448d1dfb
陈跃 <chenyue@leinao.ai>
2019-06-27 22:33:22 +0800

    调试ipaylinks支付

--- public/static/js/app/train-pay.js
+++ public/static/js/app/train-pay.js
@@ -26,7 +26,10 @@ $(function() {
   $(".pay-now-btn").on("click", function() {
     if (pay_by == "ipaylinks") {
       // http://39.105.54.233:1004/IPaylinksCTT/Payment?order_number=<这是填入CTT订单号>&email=<这里填入客户下单时的邮件地址>&phone_number=<这里填入客户下单的手机号>&ReturnURL=<支付结果返回后回调显示的URL>&Trans_ID=<客户下单后返回的交易ID>
-      var PAYURL = 'http://39.105.54.233:1004/IPaylinksCTT/Payment?order_number='+orderInfo.order_number+'&email='+orderInfo.email+'&phone_number='+orderInfo.phone_number+'&ReturnURL='+'http://127.0.0.1:8801/china-trains/train-pay.html'+'&Trans_ID='+orderInfo.user_orderid
+      var PAYURL = 'http://39.105.54.233:1004/IPaylinksCTT/Payment?order_number='+orderInfo.order_number+'&email='+orderInfo.email+'&phone_number='+orderInfo.phone_number+'&ReturnURL='+'http://182.61.175.203:8802/china-trains/train-pay.html?pay_type=paypa'+'&Trans_ID='+orderInfo.order_number;
+      // var PAYURL = 'http://39.105.54.233:1004/IPaylinksCTT/Payment?order_number='+orderInfo.order_number+'&email='+orderInfo.email+'&phone_number='+orderInfo.phone_number+'&ReturnURL='+'http://182.61.175.203:8802/china-trains/train-pay.html?pay_type=paypal'+'&Trans_ID='+orderInfo.order_number;
+      // var PAYURL = 'http://39.105.54.233:1004/IPaylinksCTT/Payment?order_number='+orderInfo.order_number+'&email='+orderInfo.email+'&phone_number='+orderInfo.phone_number+'&ReturnURL='+'http://127.0.0.1:8801/china-trains/train-pay.html'+'&Trans_ID='+orderInfo.user_orderid;
+      console.log(PAYURL);
       window.location.href = PAYURL;
     } else if (pay_by == "paypal") {
       if (!orderInfo) return false;
